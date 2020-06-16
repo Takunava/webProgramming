47877d8236780fe824b6d26aa277b846556dd164
Денис Кольцов <dkolcov@mmtr.ru>
2020-06-16 20:26:45 +0300

    добавил новую таблицу и сделал связи один ко многим и один к одному

--- app/models.py
+++ app/models.py
@@ -14,6 +14,12 @@ class Message(db.Model):
         ]
 
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
+
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(32), nullable=False)
@@ -22,10 +28,34 @@ class Tag(db.Model):
     message = db.relationship('Message', backref=db.backref('tags', lazy=True))
 
 
-class User (db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Message.query.join(
+            followers, (followers.c.followed_id == Message.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(
+            Message.timestamp.desc())
 
 
 @manager.user_loader
